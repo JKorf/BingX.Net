ba8253c3ac93783a4b7009c14a77fe979b632613
JKorf <jankorf91@gmail.com>
2024-04-26 15:40:20 +0200

    wip

---
+++ BingX.Net/BingXExchange.cs
@@ -0,0 +1,25 @@
+﻿namespace BingX.Net
+{
+    /// <summary>
+    /// BingX exchange information and configuration
+    /// </summary>
+    public static class BingXExchange
+    {
+        /// <summary>
+        /// Exchange name
+        /// </summary>
+        public static string ExchangeName => "BingX";
+
+        /// <summary>
+        /// Url to the main website
+        /// </summary>
+        public static string Url { get; } = "https://www.bingx.com";
+
+        /// <summary>
+        /// Urls to the API documentation
+        /// </summary>
+        public static string[] ApiDocsUrl { get; } = new[] {
+            "https://bingx-api.github.io/docs"
+            };
+    }
+}
