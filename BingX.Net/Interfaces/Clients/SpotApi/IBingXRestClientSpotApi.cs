aba91b23cff2000d43011880b6847a504bd61b0b
Jkorf <jankorf91@gmail.com>
2025-04-08 16:15:53 +0200

    wip

--- BingX.Net/Interfaces/Clients/SpotApi/IBingXRestClientSpotApi.cs
+++ BingX.Net/Interfaces/Clients/SpotApi/IBingXRestClientSpotApi.cs
@@ -11,20 +11,23 @@ namespace BingX.Net.Interfaces.Clients.SpotApi
         /// <summary>
         /// Endpoints related to account settings, info or actions
         /// </summary>
+        /// <see cref="IBingXRestClientSpotApiAccount"/>
         public IBingXRestClientSpotApiAccount Account { get; }
 
         /// <summary>
         /// Endpoints related to retrieving market and system data
         /// </summary>
+        /// <see cref="IBingXRestClientSpotApiExchangeData"/>
         public IBingXRestClientSpotApiExchangeData ExchangeData { get; }
 
         /// <summary>
         /// Endpoints related to orders and trades
         /// </summary>
+        /// <see cref="IBingXRestClientSpotApiTrading"/>
         public IBingXRestClientSpotApiTrading Trading { get; }
 
         /// <summary>
-        /// Get the shared rest requests client. This interface is shared with other exhanges to allow for a common implementation for different exchanges.
+        /// Get the shared rest requests client. This interface is shared with other exchanges to allow for a common implementation for different exchanges.
         /// </summary>
         public IBingXRestClientSpotApiShared SharedClient { get; }
 
