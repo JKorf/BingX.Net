7dae41c62a6f9bc00c0d7671995301b466377639
Jkorf <jankorf91@gmail.com>
2025-08-22 14:54:14 +0200

    Added ClearUserClients method to user client provider

--- BingX.Net/Clients/BingXUserClientProvider.cs
+++ BingX.Net/Clients/BingXUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace BingX.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public IBingXRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, BingXEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))
