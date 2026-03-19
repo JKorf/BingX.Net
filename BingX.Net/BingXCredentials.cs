e820b0c6d3b6c48940ce885be745dd060c1bec88
Jkorf <jankorf91@gmail.com>
2026-03-19 14:00:40 +0100

    wip

--- BingX.Net/BingXCredentials.cs
+++ BingX.Net/BingXCredentials.cs
@@ -1,4 +1,6 @@
 ﻿using CryptoExchange.Net.Authentication;
+using System;
+using System.Net;
 
 namespace BingX.Net
 {
@@ -8,12 +10,39 @@ namespace BingX.Net
     public class BingXCredentials : HMACCredential
     {
         /// <summary>
-        /// Create new credentials providing only credentials in HMAC format
+        /// Create new credentials
+        /// </summary>
+        public BingXCredentials() { }
+
+        /// <summary>
+        /// Create new credentials providing credentials in HMAC format
         /// </summary>
         /// <param name="key">API key</param>
         /// <param name="secret">API secret</param>
         public BingXCredentials(string key, string secret) : base(key, secret)
         {
+        }
+
+        /// <summary>
+        /// Create new credentials providing HMAC credentials
+        /// </summary>
+        /// <param name="credential">Credentials</param>
+        public BingXCredentials(HMACCredential credential) : base(credential.Key, credential.Secret)
+        {
+        }
+
+        /// <summary>
+        /// Specify the HMAC credentials
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="secret">API secret</param>
+        public BingXCredentials WithHMAC(string key, string secret)
+        {
+            if (!string.IsNullOrEmpty(Key)) throw new InvalidOperationException("Credentials already set");
+
+            Key = key;
+            Secret = secret;
+            return this;
         }
     }
 }
